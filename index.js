a40883b16817f4526491f768ccdb1f7b10495109
megha13 <megha.sinha13@gmail.com>
2019-03-09 21:44:23 +0100

    task 2.4 revision

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let topMovies = [
     id:'1',
     title:'Forrest Gump',
     genre:'drama',
+    director:'Robert Zemeckis',
     year:1994,
     url:'https://www.imdb.com/title/tt0109830/mediaviewer/rm1954748672'
   },
@@ -17,6 +18,7 @@ let topMovies = [
     id:'2',
     title:'Catch me if you can',
     genre:'thriller',
+    director:'Steven Spielberg',
     year:2002,
     url:'https://www.imdb.com/title/tt0264464/mediaviewer/rm3911489536'
   },
@@ -24,6 +26,7 @@ let topMovies = [
     id:'3',
     title:'The Truman show',
     genre:'drama',
+    director:'Peter Weir',
     year:1998,
     url:'https://www.imdb.com/title/tt0120382/mediaviewer/rm1927354112'
   },
@@ -31,6 +34,7 @@ let topMovies = [
     id:'4',
     title:'The Terminal',
     genre:'comedy',
+    director:'Steven Spielberg',
     year:2004,
     url:'https://www.imdb.com/title/tt0362227/mediaviewer/rm3088158464'
   },
@@ -38,6 +42,7 @@ let topMovies = [
     id:'5',
     title:'The Notebook',
     genre:'romantic',
+    director:'Nick Cassavetes',
     year:2004,
     url:'https://www.imdb.com/title/tt0332280/mediaviewer/rm1153669376'
   },
@@ -45,6 +50,7 @@ let topMovies = [
     id:'6',
     title:'Sully',
     genre:'thriller',
+    director:'Clint Eastwood',
     year:2016,
     url:'https://www.imdb.com/title/tt3263904/mediaviewer/rm1603684352'
   },
@@ -52,6 +58,7 @@ let topMovies = [
     id:'7',
     title:'Sleepless in Seattle',
     genre:'romantic',
+    director:'Nora Ephron',
     year:1993,
     url:'https://www.imdb.com/title/tt0108160/mediaviewer/rm2444975104'
   },
@@ -59,6 +66,7 @@ let topMovies = [
     id:'8',
     title:'3idiots',
     genre:'comedy',
+    director:'Rajkumar Hirani',
     year:2009,
     url:'https://www.imdb.com/title/tt1187043/mediaviewer/rm2029391104'
   },
@@ -66,6 +74,7 @@ let topMovies = [
     id:'9',
     title:'The Martian',
     genre:'adventure',
+    director:'Ridley Scott',
     year:2015,
     url:'https://www.imdb.com/title/tt3659388/mediaviewer/rm1391324160'
   },
@@ -73,12 +82,15 @@ let topMovies = [
     id:'10',
     title:'Cast Away',
     genre:'drama',
+    director:'Steven Spielberg',
     year:2000,
     url:'https://www.imdb.com/title/tt0162222/mediaviewer/rm381427456'
   }
-]
+];
+
 //directors list
-let directors = [{
+let directors = [
+  {
     id : '1',
     name : 'Robert Zemeckis',
     bio : 'https://www.imdb.com/name/nm0000709/bio?ref_=nm_ql_1',
@@ -130,13 +142,14 @@ let directors = [{
 ];
 
 // users list
-let users = [{
-  id : '1',
-  username : 'adam',
-  password : 'XXXXXX',
-  email : 'abraham@example.com',
-  dateOfBirth : 1996
-}
+let users = [
+  {
+    id : '1',
+    username : 'adam',
+    password : 'XXXXXX',
+    email : 'abraham@example.com',
+    dateOfBirth : 1996
+  }
 ];
 
 app.use(express.static('public'));
@@ -147,31 +160,33 @@ app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
 // default textual response
-app.get('/', function(req, res) {
+app.get('/', function(req, res)
+{
   res.send('Welcome to myMovie Flix');
 });
 
-
 //Get request for returning all the movies
-app.get('/movies',(req,res)=>
-{
+app.get('/movies',function(req,res)
+ {
   res.json(topMovies);
 });
 
 //get request for movie by name
-app.get('/movies/:title',(req,res) =>
-{
+app.get('/movies/:title',function(req,res)
+ {
   res.json(topMovies.find((movies)=>
   {
-    return movies.title === req.params.title
+    return movies.title === req.params.title;
   }));
 });
 //requesting movie
-app.get('/movies/genres/:genre',(req,res)=>
+app.get('/movies/genres/:genre',function(req,res)
 {
-  let genre = topMovies.filter(function(obj) {
-    return obj.genre.includes(req.params.genre.toLowerCase())
+  let genre = topMovies.filter(function(obj)
+  {
+    return obj.genre.includes(req.params.genre.toLowerCase());
   });
   if(genre.length == 0)
     res.status(404).send("Sorry No movies with this genre found in our database!..");
@@ -179,7 +194,7 @@ app.get('/movies/genres/:genre',(req,res)=>
 });
 
 //post request to add a movie
-app.post('/movies',(req,res)=>
+app.post('/movies',function(req,res)
 {
   let newMovie=req.body;
   if(!newMovie.title)
@@ -196,33 +211,33 @@ app.post('/movies',(req,res)=>
 });
 
 //delete request to delete a movie
-app.delete('/movies/:id',(req,res)=>
+app.delete('/movies/:id',function(req,res)
 {
   let movie =topMovies.find((movie)=>
   {
-    return movie.id===req.params.id
+    return movie.id===req.params.id;
   });
   if(movie)
   {
     topMovies.filter(function(obj)
     {
-      return obj.id!==req.params.id
+      return obj.id!==req.params.id;
     });
     res.status(201).send("movie " + req.params.id + " was DELETED");
   }
 });
 
 //returning data about Director by name
-app.get('/directors/:name',(req,res) =>
+app.get('/directors/:name',function(req,res)
 {
   res.json(directors.find((director)=>
   {
-    return director.name === req.params.name
+    return director.name === req.params.name;
   }));
 });
 
 //create new user
-app.post('/users/',(req,res)=>
+app.post('/users/',function(req,res)
 {
   let newUser=req.body;
   if(!newUser.username)
@@ -239,39 +254,41 @@ app.post('/users/',(req,res)=>
 });
 
 //delete requset to delete the user by id
-app.delete("/users/:id",(req,res)=>
+app.delete("/users/:id",function(req,res)
 {
   let user= users.find((user)=>
   {
-    return user.id === req.params.id
+    return user.id === req.params.id;
   });
   if(user)
   {
     users.filter(function(obj)
     {
-      return obj.id != req.params.id
+      return obj.id != req.params.id;
     });
     res.status(201).send("user"+ req.params.id +"was DELETED");
   }
 });
 
 //update the user information
-app.put("/users/:id/:username/:email/:password/:dateOfBirth",(req,res)=>{
-    let user=users.find((user) =>{
-    return user.id===req.params.id
-  });
-  if(user)
-  {
-    user.username = req.params.username;
-    user.password = req.params.password;
-    user.email = req.params.email;
-    user.dateOfBirth = req.params.dateOfBirth;
-    res.status(200).send("user information is updated");
-  }
-  else
-  {
-    res.status(404).send("user requested id" + req.params.username + "is not found");
-  }
+app.put("/users/:id/:username/:email/:password/:dateOfBirth",function(req,res)
+{
+    let user=users.find((user)=>
+    {
+    return user.id===req.params.id;
+});
+if(user)
+{
+  user.username = req.params.username;
+  user.password = req.params.password;
+  user.email = req.params.email;
+  user.dateOfBirth = req.params.dateOfBirth;
+  res.status(200).send("user information is updated");
+}
+else
+{
+  res.status(404).send("user requested id" + req.params.username + "is not found");
+}
 });
 
 // environment variable port

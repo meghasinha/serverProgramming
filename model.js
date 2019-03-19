b761de9e073ebb8c2cc0bcd93de0758ab87121bc
megha13 <megha.sinha13@gmail.com>
2019-03-19 21:26:48 +0100

    Task 2.8 updated

---
+++ model.js
@@ -0,0 +1,38 @@
+const mongoose= require('mongoose');
+
+//creating movie schema
+var movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+    Birth: String,
+    Death: String,
+    Movies: [String]
+  },
+    actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+//creating uset schema
+var userSchema=  mongoose.Schema({
+  Name: {type: String, required: true},
+  Password: { type: String, required: true},
+  Email: { type: String, required: true},
+  Birthday: Date,
+  Movies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movies'}]
+});
+
+//creating models
+var Movie= mongoose.model('Movie', movieSchema);
+var User= mongoose.model('User', userSchema);
+
+//exporting models
+module.exports.Movie= Movie;
+module.exports.User= User;
